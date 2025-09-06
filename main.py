db05b2fcc7e411f2606682507f83d61b5d3753e9
TheDeliverator <deliverator@deliverator.online>
2025-09-05 19:39:02 -0500

    Update game loop to use object containers

--- main.py
+++ main.py
@@ -10,18 +10,27 @@ def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
+    # main game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        screen.fill("black")
+        screen.fill("black") # clear the screen each frame
 
-        player.update(dt)
-        player.draw(screen)
+        # update and draw all objects  
+        updatable.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen)
+        
+        # flip the display and tick the clock
         pygame.display.flip()
         clock.tick(60)
         dt = clock.get_time() / 1000
